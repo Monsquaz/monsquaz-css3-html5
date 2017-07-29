eb944baaeb8a5f3903ad10a53fc757c1a63667a9
Patrik Weibull <patrik.weibull@gmail.com>
2017-07-29 14:02:50 +0000

    Various shit

--- gulpfile.js
+++ gulpfile.js
@@ -1,10 +1,11 @@
-let gulp    = require('gulp');
-let cssnano = require('gulp-cssnano');
-let sass    = require('gulp-sass');
+let gulp            = require('gulp');
+let cssnano         = require('gulp-cssnano');
+let sass            = require('gulp-sass');
+let moduleImporter  = require('sass-module-importer');
 
-gulp.task('default', function(){
-  return gulp.src(['*.scss'])
-    .pipe(sass())
+gulp.task('css', function(){
+  return gulp.src(['monsquaz.scss'])
+    .pipe(sass({ importer: moduleImporter() }))
     .pipe(cssnano({
       autoprefixer: {
         browsers: [
@@ -19,4 +20,8 @@ gulp.task('default', function(){
       }
     }))
     .pipe(gulp.dest('css'));
+});
+
+gulp.task('default', function() {
+  gulp.watch('*.scss', ['css']);
 });
